e409a9eb62fc66b8b7c92c3940c5dc4b29576f4a
Leonid Vygovskiy <leonid.vygovskiy@gmail.com>
2015-11-11 23:26:13 +0300

    Adds timeout of subscribers waiting

--- app/controllers/HttpMediator.scala
+++ app/controllers/HttpMediator.scala
@@ -1,10 +1,12 @@
 package controllers
 
 import org.slf4j.LoggerFactory
-import play.api.libs.iteratee.{Enumeratee, Concurrent}
+import play.api.libs.concurrent.Akka
+import play.api.libs.iteratee.{Iteratee, Enumerator, Enumeratee, Concurrent}
 import play.api.mvc._
-
+import scala.concurrent.duration._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.Play.current
 
 /**
  * Created by leonidv on 16.10.15.
@@ -15,24 +17,44 @@ class HttpMediator extends Controller {
   val log = LoggerFactory.getLogger(classOf[HttpMediator])
 
   def subscribe(token : String) = Action { request =>
-    log.debug("subscribe " + request.id)
-    Ok.feed(out &> filter(token) &> extractBody())
+//    log.debug("subscribe " + request.id + ", token = " + token)
+    Akka.system.scheduler.scheduleOnce(7 seconds) {
+      in.push(new BreakMessage(token))
+    }
+
+    //Ok.feed(out &> break &> disconnected(request)  &> extractBody())
+
+    Ok.feed(out &> disconnected(request) &> filter(token) &> checkTimout &> extractBody())
   }
 
   def publish(token : String) = Action(parse.tolerantText){ request =>
     val body = request.body
-    log.debug(s"publish ${request.id}, body = $body")
-    in.push(Message(token, body))
+//    log.debug(s"publish ${request.id}, token = $token, body = $body")
+    in.push(new TextMessage(token, body))
     Ok(":)\n")
   }
 }
 
-case class Message(val token : String, val body : String)
+sealed abstract class Message(val token : String)
+class TextMessage(token : String, val body : String) extends Message(token)
+class BreakMessage(token: String) extends Message(token)
 
 object HttpMediator {
+  val log = LoggerFactory.getLogger(classOf[HttpMediator])
+
   val (out, in) = Concurrent.broadcast[Message]
 
-  def extractBody() : Enumeratee[Message,String] = Enumeratee.map[Message] (msg => msg.body)
+  def extractBody() : Enumeratee[Message,String] = Enumeratee.map[Message]{
+    case msg : TextMessage => msg.body
+    case break : BreakMessage => ""
+  }
 
   def filter(token : String) : Enumeratee[Message,Message] = Enumeratee.filter(msg => msg.token.equals(token))
+
+  def disconnected(request : Request[_]): Enumeratee[Message, Message] =
+    Enumeratee.onIterateeDone{()=>
+      //log.info("disconnected, request.id = "+request.id)
+    }
+
+  def checkTimout : Enumeratee[Message,Message] = Enumeratee.breakE[Message](x => x.isInstanceOf[BreakMessage])
 }
